2d88a7bdcf8ae34cd3b7669f2553a1acb515740d
bandura <banduraweb@ukr.net>
2020-01-20 02:03:33 +0200

    Updates

---
+++ precache-manifest.d36e66551b0485b24d564670cf9fe555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545088f299a90ade13f96d4b7fe2bb82",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "ce3aa5e1cd3b6c264bc6",
+    "url": "/react_uber-eats/static/css/main.991e6ccc.chunk.css"
+  },
+  {
+    "revision": "b70fd21f8fe7f4520481",
+    "url": "/react_uber-eats/static/js/2.ae96db24.chunk.js"
+  },
+  {
+    "revision": "ce3aa5e1cd3b6c264bc6",
+    "url": "/react_uber-eats/static/js/main.d62f7702.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.d36e66551b0485b24d564670cf9fe555.js"
 );
 
 self.addEventListener('message', (event) => {
