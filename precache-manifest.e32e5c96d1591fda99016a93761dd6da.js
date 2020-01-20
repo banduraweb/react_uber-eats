9b473ad07a4d3d30892a362d57f925c67982f1cc
bandura <banduraweb@ukr.net>
2020-01-20 12:09:07 +0200

    Updates

--- precache-manifest.e32e5c96d1591fda99016a93761dd6da.js
+++ precache-manifest.e32e5c96d1591fda99016a93761dd6da.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "39bfdb7f7aadc736706005ac85a1ff6e",
+    "revision": "0a333fb6a7029a27c68e4d9f39e9c6de",
     "url": "/react_uber-eats/index.html"
   },
   {
-    "revision": "ad0e8203b96f4c62f359",
+    "revision": "2ba1f8bc08338b313cb7",
     "url": "/react_uber-eats/static/css/main.c0acb45c.chunk.css"
   },
   {
-    "revision": "b70fd21f8fe7f4520481",
-    "url": "/react_uber-eats/static/js/2.ae96db24.chunk.js"
+    "revision": "a3c12ab28de2d22859ab",
+    "url": "/react_uber-eats/static/js/2.bda7b912.chunk.js"
   },
   {
-    "revision": "ad0e8203b96f4c62f359",
-    "url": "/react_uber-eats/static/js/main.d62f7702.chunk.js"
+    "revision": "2ba1f8bc08338b313cb7",
+    "url": "/react_uber-eats/static/js/main.86cbfe7e.chunk.js"
   },
   {
     "revision": "183d5977488a46d51899",
